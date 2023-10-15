5b31874925fd9c836aab802a54efb3ea2d421415
kazukiyoda7 <yodakazu@icloud.com>
2023-10-16 04:39:37 +0900

    update

--- logger.py
+++ logger.py
@@ -164,8 +164,11 @@ def load_networks(networks, result_dir, name='', loss='', criterion=None):
 
 # --------------------------------------------------------------------------------------------------------
 # seed を固定する関数
-def fix_seed(seed):
-    torch.manual_seed(seed)
-    np.random.seed(seed)
+def fix_seed(seed):    
     random.seed(seed)
-    torch.backends.cudnn.benchmark = True
+    np.random.seed(seed)
+    torch.manual_seed(seed) 
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
